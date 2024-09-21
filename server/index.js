810916c0b00482b548e62e36a9fadc094ca07e91
sivasindhu <sindhusiva191@gmail.com>
2024-09-21 23:36:21 +0530

    error

--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ import dotenv from "dotenv";
 
 import postRouter from "./routes/posts.js";
 
-const app = express();
 dotenv.config();
+const app = express();
+
 
 app.use(bodyParser.json( {limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded( {limit: "30mb", extended: true }));
@@ -16,14 +17,12 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("hello to Memories API");
-}); 
+});
 
 app.use('/posts', postRouter);
 const PORT = process.env.PORT || 5000;
 
-const url = process.env.CONNECTION_URL;
-
-mongoose.connect(url).then(()=> app.listen(PORT, ()=>
+mongoose.connect(process.env.CONNECTION_URL).then(()=> app.listen(PORT, ()=>
     console.log(`server running on port: ${PORT}`)
 ))
 .catch((error)=> console.log(error.message));
